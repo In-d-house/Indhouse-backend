9e23b0a7156a4e29cc446dfa0f48bbaa131f50c0
yunsu <59559334+emune216@users.noreply.github.com>
2021-05-08 01:07:46 +0900

    🚀 feat: express setting

--- app.js
+++ app.js
@@ -1,41 +1,37 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const createError = require("http-errors");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const { port } = require("./configs");
+const initLoaders = require("./loaders");
 
-var app = express();
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+initLoaders(app);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === "production") {
+    console.log(err);
+    if (err instanceof mongoose.Error) err = createError(500);
+    err.stack = null;
+  }
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json(err);
+});
+
+app.listen(port || 5000, () => {
+  console.log(`Server is running on ${port || 5000}`);
 });
 
 module.exports = app;
