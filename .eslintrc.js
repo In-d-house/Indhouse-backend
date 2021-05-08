913cadf01cca8f5f2b3a0c6d12c9d89b419d7289
yunsu <59559334+emune216@users.noreply.github.com>
2021-05-08 15:10:33 +0900

    🚀 feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/* eslint-disable quote-props */
+module.exports = {
+  "env": {
+    "browser": true,
+    "commonjs": true,
+    "es2021": true,
+  },
+  "extends": ["eslint:recommended", "airbnb-base"],
+  "parserOptions": {
+    "ecmaVersion": 12,
+  },
+  "rules": {
+    eqeqeq: ["error", "always"],
+    quotes: ["error", "double", { "allowTemplateLiterals": true }],
+    "quote-props": ["error", "consistent"],
+    "no-unused-vars": "warn",
+    "no-console": 0,
+    "no-param-reassign": 0,
+  },
+};
--- models/Music.js
+++ models/Music.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const { ObjectId } = mongoose.Schema.Types;
 
 const musicSchema = new mongoose.Schema({
   title: {
